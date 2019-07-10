f7d23531ded06f3d33c21a187b5006556dd9774d
jeff-lund <jdl3@pdx.edu>
2019-07-10 12:22:52 -0700

    fixed brounkers

--- Python/Loops/bronk.py
+++ Python/Loops/bronk.py
@@ -1,11 +1,10 @@
-def bronk1(n):
-    #n = int(input("Enter a positive, odd number: "))
-    if n % 2 == 0:
-        print("Nope")
-    else:
-        z = 1
-        while n > 1:
-            z = 2 + n**2 / z
-            n -= 2
-        z = 4 / (1 + 1 / z)
-        print(z)
+n = int(input("Enter a positive, odd number: "))
+if n % 2 == 0:
+    print("Nope")
+else:
+    z = 1
+    while n > 1:
+        z = 2 + n**2 / z
+        n -= 2
+    z = 4 / (1 + 1 / z)
+    print(z)
