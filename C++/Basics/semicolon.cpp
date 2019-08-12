1ba2fbe11280c7509072f22a4b8aa0e674c2e962
jeff-lund <jdl3@pdx.edu>
2019-08-12 15:12:27 -0700

    added bad statement

---
+++ C++/Basics/semicolon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int x;
+    
+    cin >> x;
+    if(x > 10);
+    {
+        cout << "x is big!" << endl;
+    }
+
+    return 0;
+}
